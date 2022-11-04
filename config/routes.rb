354136f1afddb4a4693a5a63b8b827c0f9133db9
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-04 12:44:02 +0000

    [Add]必要なファイルの設定/作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+   root 'homes#top'
+   resource :book, only: [:show, :index, :edit]
+   resource :user, only: [:show, :index, :edit]
 end
