35cbf616436b4707e4cd089d85cc5e9c3b7cc550
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-05 10:39:12 +0000

    [Add]headerレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'homes#top'
-   resource :book, only: [:show, :index, :edit]
-   resource :user, only: [:show, :index, :edit]
+   get "home/about" => "homes#about"
+   resources :book, only: [:show, :index, :edit]
+   resources :user, only: [:show, :index, :edit]
 end
