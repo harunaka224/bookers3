91f859883e1dc0b2068f178056d21be2869ca84d
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-06 11:15:32 +0000

    [Add]footerレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'homes#top'
    get "home/about" => "homes#about"
-   resources :book, only: [:show, :index, :edit]
-   resources :user, only: [:show, :index, :edit]
+   resources :books, only: [:show, :index, :edit]
+   resources :users, only: [:show, :index, :edit]
 end
