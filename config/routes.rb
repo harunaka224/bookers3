006dfcaf15c7675d87b42c0fa3636ee79d1ec0d8
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-02 10:09:44 +0000

    [Add]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'homes#top'
    get "home/about" => "homes#about"
+   post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
+   
    resources :books, only: [:show, :index, :edit, :create, :new]
    resources :users, only: [:show, :index, :edit]
 end
