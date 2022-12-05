9cba68e0a0500cbc43bafa95bf44649c184f66ae
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-05 12:17:42 +0000

    [Fix]コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,13 @@ class CommentsController < ApplicationController
     comment = current_user.comments.new(comment_params)
     comment.book_id = book.id
     comment.save
+    flash[:notice] = '投稿にコメントしました。'
     redirect_to request.referer
   end
 
   def destroy
     Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    flash[:notice] = 'コメントを削除しました。'
     redirect_to request.referer
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'homes#top'
   get "home/about" => "homes#about"
   post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
-   
+
   resources :users, only: [:show, :index, :edit]
-  
+
   resources :books, only: [:show, :index, :edit, :create, :new] do
     resource :favorites, only: [:create, :destroy]
-    resource :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   end
-  
+
 end
