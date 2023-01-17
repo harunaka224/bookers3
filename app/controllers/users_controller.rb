e5e0f952ce1c64eb114f844a013dc26f88314889
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2023-01-17 09:11:30 +0000

    [Fix]ゲストログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
   def new
     @user = User.new
   end
@@ -6,21 +8,28 @@ class UsersController < ApplicationController
   def create
   @user = User.new(user_params)
   end
-  
+
   def show
   end
 
   def index
-   
+
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   private
-  
+
   def user_params
   params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root 'homes#top'
   get "home/about" => "homes#about"
-  post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
-
   resources :users, only: [:show, :index, :edit]
+
+  #guestuser機能
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   resources :books, only: [:show, :index, :edit, :create, :new] do
     resource :favorites, only: [:create, :destroy]
